3d04c0c08cb669ffefa557d4c66d3b986a22a8be
Sunit Ghosh <engineersunit@gmail.com>
2024-05-17 15:54:38 +0530

    LLM Chatbot with fixed input text

---
+++ MyLlmChatbotFixedInput.py
@@ -0,0 +1,13 @@
+from langchain_community.llms import OCIGenAI
+
+endpoint = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
+
+llm = OCIGenAI(
+    model_id="cohere.command",
+    service_endpoint=endpoint,
+    compartment_id="test_compartment_sunitghosh",
+    model_kwargs={"max_tokens": 256}
+)
+
+response = llm.invoke("Who is Sunit Ghosh", temperature=0.5)
+print("Fixed Text Response => " + response)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # gen-ai-llm-app
+
 Gen AI Application based on LLM
+
+## Prerequisites
+
+Install these python packages
+
+- langchain
+- langchain_community
+- oci (Oracle Cloud Infrastructure Python SDK)
+
